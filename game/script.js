26c49ceab97bba3856cbbed9d099a7933fb26b7a
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-06-02 22:29:45 +0100

    Add toggleImage function to show/hide movie image

--- game/script.js
+++ game/script.js
@@ -1,20 +1,5 @@
-// Get the canvas element
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
-
-// Set the puzzle dimensions
-const puzzleWidth = canvas.width;
-const puzzleHeight = canvas.height;
-
-// Define the number of rows and columns in the puzzle
-const numRows = 3;
-const numCols = 3;
-
-// Load the image
-const image = new Image();
-image.src = 'the-wind-rises.png';
-image.addEventListener('load', start);
-
-// Initialize the puzzle pieces
-let puzzlePieces = [];
+function toggleImage() {
+    var movieImg = document.getElementById("movie-img");
+    movieImg.classList.toggle("hidden"); // Show or hide the movie image
+  }
 
