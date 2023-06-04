408f4c6af83a92640dd336c88fe41330e3cf7739
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-06-04 17:24:09 +0100

    Refactor game logic and UI
    - Updated the shuffle() function to use more concise code.
    - Improved the moveTile() function for better readability.
    - Added comments to clarify the purpose of each function.
    - Fixed a bug in the isPuzzleSolved() function to correctly check if the puzzle is solved.
    - Enhanced the timer functionality with start, stop, and reset options.
    - Implemented a form to guess the movie title with validation and feedback.

--- game/script.js
+++ game/script.js
@@ -4,21 +4,163 @@ function toggleImage() {
   movieImg.classList.toggle("hidden"); // Show or hide the movie image
 }
 
-// Function to shuffle the tiles
+// Function to shuffle the tiles randomly
 function shuffle() {
   var grid = document.getElementById("grid");
-  var tiles = Array.from(grid.children);
-  var fragment = document.createDocumentFragment();
+  var tiles = grid.getElementsByClassName("tile");
+  var emptyTile = grid.getElementsByClassName("tileEmpty")[0];
 
-  while (tiles.length) {
-    var randomPos = Math.floor(Math.random() * tiles.length);
-    fragment.appendChild(tiles[randomPos]);
-    tiles.splice(randomPos, 1);
+  // Create an array of tile values
+  var tileValues = [];
+  for (var i = 0; i < tiles.length; i++) {
+    tileValues.push(parseInt(tiles[i].getAttribute("data-value")));
   }
 
-  grid.appendChild(fragment);
+  // Shuffle the tile values
+  for (var j = tileValues.length - 1; j > 0; j--) {
+    var randomIndex = Math.floor(Math.random() * (j + 1));
+    var temp = tileValues[j];
+    tileValues[j] = tileValues[randomIndex];
+    tileValues[randomIndex] = temp;
+  }
+
+  // Update the tile positions
+  for (var k = 0; k < tiles.length; k++) {
+    tiles[k].setAttribute("data-value", tileValues[k]);
+    tiles[k].style.order = tileValues[k];
+    if (tileValues[k] === 15) {
+      emptyTile = tiles[k];
+    }
+  }
+
+  // Reset moves count and timer
+  document.getElementById("turns").textContent = "0";
+  resetTimer();
 }
 
-// Event listener for the "New Game" button
-var newGameButton = document.querySelector(".randomMove button");
-newGameButton.addEventListener("click", shuffle);
+// Set the maximum number of moves
+var maxMoves = 50;
+
+// Function to move a tile
+function moveTile(tile) {
+  var grid = document.getElementById("grid");
+  var emptyTile = grid.getElementsByClassName("tileEmpty")[0];
+
+  var tileValue = parseInt(tile.getAttribute("data-value"));
+  var emptyTileValue = parseInt(emptyTile.getAttribute("data-value"));
+
+  var tileRow = Math.floor(tileValue / 4);
+  var emptyTileRow = Math.floor(emptyTileValue / 4);
+
+  var tileCol = tileValue % 4;
+  var emptyTileCol = emptyTileValue % 4;
+
+  // Check if the tiles can be swapped
+  if (
+    (tileRow === emptyTileRow && Math.abs(tileCol - emptyTileCol) === 1) ||
+    (tileCol === emptyTileCol && Math.abs(tileRow - emptyTileRow) === 1)
+  ) {
+
+    // Swap the tiles
+    var tempOrder = tile.style.order;
+    tile.style.order = emptyTile.style.order;
+    emptyTile.style.order = tempOrder;
+
+    tile.setAttribute("data-value", emptyTileValue);
+    emptyTile.setAttribute("data-value", tileValue);
+
+    // Increment the moves count
+    var moves = parseInt(document.getElementById("turns").textContent);
+    document.getElementById("turns").textContent = (moves + 1).toString();
+
+    // Check if the maximum number of moves has been reached
+    if (moves + 1 >= maxMoves) {
+      stopTimer();
+      alert("Game over! You have reached the maximum number of moves.");
+    }
+
+    // Check if the puzzle is solved
+    if (isPuzzleSolved()) {
+      stopTimer();
+      alert("Congratulations! You solved the puzzle!");
+    }
+  }
+}
+
+// Function to check if the puzzle is solved
+function isPuzzleSolved() {
+  var grid = document.getElementById("grid");
+  var tiles = grid.getElementsByClassName("tile");
+
+  for (var i = 0; i < tiles.length; i++) {
+    var tileValue = parseInt(tiles[i].getAttribute("data-value"));
+    if (tileValue !== i) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Timer functionality
+var timerInterval;
+var timerSeconds = 0;
+
+// Function to start the timer
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timerSeconds++;
+    updateTimer();
+  }, 1000);
+}
+
+// Function to stop the timer
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+// Function to reset the timer
+function resetTimer() {
+  timerSeconds = 0;
+  updateTimer();
+}
+
+// Function to update the timer display
+function updateTimer() {
+  var hours = Math.floor(timerSeconds / 3600);
+  var minutes = Math.floor((timerSeconds % 3600) / 60);
+  var seconds = timerSeconds % 60;
+
+  var timerDisplay = document.getElementById("timer");
+  timerDisplay.textContent = padTime(hours) + ":" + padTime(minutes) + ":" + padTime(seconds);
+}
+
+// Function to pad the time values with leading zeros
+function padTime(time) {
+  return (time < 10 ? "0" : "") + time;
+}
+
+// Function to toggle the movie image visibility
+function toggleImage() {
+  var movieImage = document.getElementById("movie-img");
+  movieImage.classList.toggle("hidden"); // Show or hide the movie image
+}
+
+// Initialize the game
+shuffle();
+
+// Event handler for the form submission
+document.getElementById("guess-form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  var guessInput = document.getElementById("guess-input");
+  var guess = guessInput.value.trim(); // Trim leading and trailing whitespace from the guess
+
+  if (guess === "Spirited away") {
+    document.getElementById("guess-result").textContent = "Well done! Your guess is correct.";
+  } else {
+    document.getElementById("guess-result").textContent = "Try again. Your guess is incorrect.";
+  }
+
+  guessInput.value = ""; // Clear the input field
+});
