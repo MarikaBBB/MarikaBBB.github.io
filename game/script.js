6bfe9f780dfbc158995b4e5aa8939dcc81dd526b
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-06-04 12:05:06 +0100

    feat - Create event listener for "New Game" button to shuffle tiles

--- game/script.js
+++ game/script.js
@@ -1,5 +1,24 @@
+// Function to toggle the movie image visibility
 function toggleImage() {
-    var movieImg = document.getElementById("movie-img");
-    movieImg.classList.toggle("hidden"); // Show or hide the movie image
+  var movieImg = document.getElementById("movie-img");
+  movieImg.classList.toggle("hidden"); // Show or hide the movie image
+}
+
+// Function to shuffle the tiles
+function shuffle() {
+  var grid = document.getElementById("grid");
+  var tiles = Array.from(grid.children);
+  var fragment = document.createDocumentFragment();
+
+  while (tiles.length) {
+    var randomPos = Math.floor(Math.random() * tiles.length);
+    fragment.appendChild(tiles[randomPos]);
+    tiles.splice(randomPos, 1);
   }
 
+  grid.appendChild(fragment);
+}
+
+// Event listener for the "New Game" button
+var newGameButton = document.querySelector(".randomMove button");
+newGameButton.addEventListener("click", shuffle);
